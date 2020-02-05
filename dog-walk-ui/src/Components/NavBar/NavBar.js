2d09bcd740ef3bca807770e272b174165cf0d8a5
Matt Gill <mtgill90@outlook.com>
2020-02-04 20:39:37 -0600

    logout and routes working

--- dog-walk-ui/src/Components/NavBar/NavBar.js
+++ dog-walk-ui/src/Components/NavBar/NavBar.js
@@ -10,13 +10,20 @@ import {
   NavLink,
 } from 'reactstrap';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 const NavBar = () => {
+  const logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
   return (
     <>
     <Navbar color="dark" dark expand="md">
           <NavbarBrand href="/">Should I Walk My Dog?</NavbarBrand>
             <Nav className="ml-auto" navbar>
-            <button className="btn btn-danger my-2 my-sm-0 ml-3" >Log Out</button>
+            <button className="btn btn-danger my-2 my-sm-0 ml-3" onClick={logMeOut}>Log Out</button>
             </Nav>
         </Navbar>
     </>
